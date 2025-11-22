4b516683ad2eb075488a8371879e3626ff59c165
Fastre <61952761+IamFastre@users.noreply.github.com>
2025-11-22 04:50:10 +0200

    chore: file organizery

--- src/lib/client/storage/index.ts
+++ src/lib/client/storage/index.ts
@@ -1,5 +1,3 @@
-import { browser } from "$app/environment";
-
 import { keys } from "$/utils/funcs";
 
 import { DefaultLocalStorageEntries } from "./default";
@@ -41,32 +39,4 @@ export class LocalStorage {
   static reset<T extends keyof LocalStorageEntries>(key: T): boolean {
     return this.set(key, DefaultLocalStorageEntries[key]);
   }
-}
-
-export class StyleTheme {
-  static get name(): string | null {
-    if (!browser)
-      return null;
-
-    const html = document.documentElement;
-    return html.getAttribute('data-theme');
-  }
-
-  static property(category: string, name: string): string | null {
-    if (!browser)
-      return null;
-
-    const html = document.documentElement;
-    const style = getComputedStyle(html);
-    const value = style.getPropertyValue(`--fussr-${category}-${name}`);
-
-    if (value.length)
-      return value;
-
-    console.warn(`Theme property '${category}:${name}' does not exist.`);
-    return null;
-  }
-
-  static color(name: string) { return this.property('color', name); }
-  static value(name: string) { return this.property('value', name); }
 }
---
+++ src/lib/client/styles/index.ts
@@ -0,0 +1,29 @@
+import { browser } from "$app/environment";
+
+export class StyleTheme {
+  static get name(): string | null {
+    if (!browser)
+      return null;
+
+    const html = document.documentElement;
+    return html.getAttribute('data-theme');
+  }
+
+  static property(category: string, name: string): string | null {
+    if (!browser)
+      return null;
+
+    const html  = document.documentElement;
+    const style = getComputedStyle(html);
+    const value = style.getPropertyValue(`--fussr-${category}-${name}`);
+
+    if (value.length)
+      return value;
+
+    console.warn(`Theme property '${category}:${name}' does not exist.`);
+    return null;
+  }
+
+  static color(name: string) { return this.property('color', name); }
+  static value(name: string) { return this.property('value', name); }
+}
