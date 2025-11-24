b061a32b3c20be7c41520b2f296469539491dcbc
Fastre <61952761+IamFastre@users.noreply.github.com>
2025-11-24 04:19:25 +0200

    feat: temp link to ask page

--- src/routes/(app)/(home)/+page.svelte
+++ src/routes/(app)/(home)/+page.svelte
@@ -17,29 +17,38 @@
     <span>
       {m.welcome({ name:m.fussr() })}
     </span>
-    <ButtonGroup class="group">
+    <div class="buttons">
+      <ButtonGroup class="group">
+        {#if data.auth.isSigned}
+          <Link variant="wrapper" href="/me">
+            <Button>
+              {m.routes_me()}
+            </Button>
+          </Link>
+          <Button onclick={onSignOut}>
+            {m.auth_sign_out()}
+          </Button>
+        {:else}
+          <Link variant="wrapper" href="/auth/sign-in">
+            <Button>
+              {m.auth_sign_in()}
+            </Button>
+          </Link>
+          <Link variant="wrapper" href="/auth/sign-up">
+            <Button>
+              {m.auth_sign_up()}
+            </Button>
+          </Link>
+        {/if}
+      </ButtonGroup>
       {#if data.auth.isSigned}
-        <Link variant="wrapper" href="/me">
+        <Link variant="wrapper" href="/ask">
           <Button>
-            {m.routes_me()}
-          </Button>
-        </Link>
-        <Button onclick={onSignOut}>
-          {m.auth_sign_out()}
-        </Button>
-      {:else}
-        <Link variant="wrapper" href="/auth/sign-in">
-          <Button>
-            {m.auth_sign_in()}
-          </Button>
-        </Link>
-        <Link variant="wrapper" href="/auth/sign-up">
-          <Button>
-            {m.auth_sign_up()}
+            Ask
           </Button>
         </Link>
       {/if}
-    </ButtonGroup>
+    </div>
   </Panel>
 </div>
 
@@ -62,8 +71,12 @@
       padding: 50px;
       gap: 50px;
 
-      .titchy.button-group.group {
-        direction: ltr;
+      .buttons {
+        gap: 10px;
+
+        .titchy.button-group.group {
+          direction: ltr;
+        }
       }
     }
   }
