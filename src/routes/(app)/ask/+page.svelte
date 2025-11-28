32b4f583ece0e143c46587140426991c60f89aa3
Fastre <61952761+IamFastre@users.noreply.github.com>
2025-11-28 14:53:03 +0200

    chore: tweaks to the ask page

--- src/routes/(app)/ask/+page.svelte
+++ src/routes/(app)/ask/+page.svelte
@@ -60,213 +60,207 @@
   };
 </script>
 
-<div class="ask">
-  <div class="form">
-    <Panel>
-      <h2>
-        <CircleQuestionMark />
-        {m.ask_ask_question()}
-      </h2>
-      <Label for="title">
-        <InputWrapper label={m.ask_title()}>
-          <Input
-            bind:value={titleInput}
-            id="title"
-            placeholder={m.ask_title_placeholder()}
-            disabled={loading}
-          />
-        </InputWrapper>
-      </Label>
+<Panel class="form">
+  <h2>
+    <CircleQuestionMark />
+    {m.ask_ask_question()}
+  </h2>
+  <Label for="title">
+    <InputWrapper label={m.ask_title()}>
+      <Input
+        bind:value={titleInput}
+        id="title"
+        placeholder={m.ask_title_placeholder()}
+        disabled={loading}
+      />
+    </InputWrapper>
+  </Label>
 
-      <Label for="body">
-        <InputWrapper
-          label={m.ask_body()}
-          side-actions={{ clearable:'always', pastable:'hover' }}
-        >
-          <Textarea
-            bind:value={bodyInput}
-            id="body"
-            placeholder={m.ask_body_placeholder()}
-            disabled={loading}
-          />
-        </InputWrapper>
-      </Label>
+  <Label for="body">
+    <InputWrapper
+      label={m.ask_body()}
+      side-actions={{ clearable:'always', pastable:'hover' }}
+    >
+      <Textarea
+        bind:value={bodyInput}
+        id="body"
+        placeholder={m.ask_body_placeholder()}
+        disabled={loading}
+      />
+    </InputWrapper>
+  </Label>
 
-      <Label class={["tags-label", { disabled:loading }]} for="tags">
-        <Panel variant="secondary">
-          <span class="label-title">{m.ask_tags()}</span>
-          <span class="label-desc">{m.ask_tags_description()}</span>
-          <div class="tag-array">
-            {#each tags as tag, i (i)}
-              <div transition:scale>
-                <Button class="tag" variant="wrapper" onclick={remTag(tag)} disabled={loading}>
-                  <span>#{tag}</span>
-                  <X />
-                </Button>
-              </div>
-            {:else}
-              <div class="no-tag">
-                <Ban />
-                <span>{m.ask_tags_empty()}</span>
-              </div>
-            {/each}
-            {#if tags.length}
-              <div class="sep"></div>
-              <Button variant="wrapper" onclick={() => tags = []} disabled={loading}>
-                <X />
-              </Button>
-            {/if}
-          </div>
-          <div class="input-row">
-            <Input
-              bind:value={tagInput}
-              id="tags"
-              placeholder={m.ask_tags_placeholder()}
-              disabled={tags.length >= 6 || loading}
-              onkeydown={e => {
-                if (e.key === 'Enter')
-                  addTag()
-
-                if (e.key === 'Backspace' && tagInput.length === 0)
-                  if (e.shiftKey) remTag(tags[0])()
-                  else remTag(tags[tags.length - 1])()
-              }}
-            />
-            <Button onclick={addTag} disabled={tags.length >= 6 || loading}>
-              <Plus />
+  <Label class={["tags-label", { disabled:loading }]} for="tags">
+    <Panel variant="secondary">
+      <span class="label-title">{m.ask_tags()}</span>
+      <span class="label-desc">{m.ask_tags_description()}</span>
+      <div class="tag-array">
+        {#each tags as tag, i (i)}
+          <div transition:scale>
+            <Button class="tag" variant="wrapper" onclick={remTag(tag)} disabled={loading}>
+              <span>#{tag}</span>
+              <X />
             </Button>
           </div>
-        </Panel>
-      </Label>
+        {:else}
+          <div class="no-tag">
+            <Ban />
+            <span>{m.ask_tags_empty()}</span>
+          </div>
+        {/each}
+        {#if tags.length}
+          <div class="sep"></div>
+          <Button variant="wrapper" onclick={() => tags = []} disabled={loading}>
+            <X />
+          </Button>
+        {/if}
+      </div>
+      <div class="input-row">
+        <Input
+          bind:value={tagInput}
+          id="tags"
+          placeholder={m.ask_tags_placeholder()}
+          disabled={tags.length >= 6 || loading}
+          onkeydown={e => {
+            if (e.key === 'Enter')
+              addTag()
 
-      <Separator variant="secondary" />
-
-      <div class="actions">
-        <Button class="big" scaling={false} onclick={onSubmit} disabled={loading}>
-          {m.ask_submit()}
-        </Button>
-        <Button class="small" variant="secondary" scaling={false} disabled={loading}>
-          <Archive />
-          <span class="title">
-            {m.ask_draft()}
-          </span>
+            if (e.key === 'Backspace' && tagInput.length === 0)
+              if (e.shiftKey) remTag(tags[0])()
+              else remTag(tags[tags.length - 1])()
+          }}
+        />
+        <Button onclick={addTag} disabled={tags.length >= 6 || loading}>
+          <Plus />
         </Button>
       </div>
     </Panel>
+  </Label>
+
+  <Separator variant="secondary" />
+
+  <div class="actions">
+    <Button class="big" scaling={false} onclick={onSubmit} disabled={loading}>
+      {m.ask_submit()}
+    </Button>
+    <Button class="small" variant="secondary" scaling={false} disabled={loading}>
+      <Archive />
+      <span class="title">
+        {m.ask_draft()}
+      </span>
+    </Button>
   </div>
-</div>
+</Panel>
 
 <style lang="scss">
   @use "@/styles/utils.scss" as *;
 
-  .ask {
-    .form {
-      @include wide-screen { margin: 0 7.5%; }
+  :global
+  .titchy.panel.form {
+    flex: 1;
 
-      h2 {
-        @include flexbox(false, row, center, $gap: 15px);
-        margin: 15px 0;
+    > h2 {
+      @include flexbox(false, row, center, $gap: 15px);
+      margin-bottom: 15px;
 
-        :global svg { @include size(1.2em); color: C(accent); }
+      svg { @include size(1.2em); color: C(accent); }
+    }
+
+    > .titchy.label {
+      .label-title {
+        color: C(accent);
+        font-weight: bold;
       }
 
-      :global
-      .titchy.label {
-        .label-title {
-          color: C(accent);
-          font-weight: bold;
-        }
-
-        .label-desc {
-          color: C(secondary, 15%);
-          font-weight: 500;
-          font-size: smaller;
-        }
-
-        .input-row {
-          flex-direction: row;
-          align-items: center;
-          gap: 10px;
-
-          .input { flex: 1; }
-        }
-
-        textarea.titchy.input {
-          height: 350px;
-          max-height: 100dvh;
-        }
-
-        &.tags-label {
-          &.disabled { opacity: 0.5; }
-
-          .tag-array {
-            font-size: smaller;
-            gap: 7.5px;
-            padding: 7.5px;
-            border-radius: 10px;
-            background-color: C(accent, 5%);
-            flex-direction: row;
-            flex-wrap: wrap;
-            align-items: center;
-
-            .titchy.button.tag {
-              flex-direction: row;
-              align-items: center;
-              gap: 0.5ch;
-              color: C(accent);
-              padding: 5px;
-              border-radius: 10px;
-              background-color: C(accent, 10%);
-
-              svg { @include size(1em); }
-            }
-
-            .no-tag {
-              flex-direction: row;
-              align-items: center;
-              gap: 1ch;
-              color: C(secondary);
-              opacity: 0.3;
-              font-style: italic;
-            }
-
-            .sep { flex: 1; }
-          }
-        }
+      .label-desc {
+        color: C(secondary, 15%);
+        font-weight: 500;
+        font-size: smaller;
       }
 
-      .actions {
+      .input-row {
         flex-direction: row;
-        gap: 0.75em;
+        align-items: center;
+        gap: 10px;
 
-        :global
-        .titchy.button {
-          svg { @include size(1.5em, 'all'); }
+        .input { flex: 1; }
+      }
 
-          &.big {
-            width: 175px;
+      textarea.titchy.input {
+        height: 350px;
+        max-height: 100dvh;
+        font-family: FiraCode;
+      }
+
+      &.tags-label {
+        &.disabled { opacity: 0.5; }
+
+        .tag-array {
+          font-size: smaller;
+          gap: 7.5px;
+          padding: 7.5px;
+          border-radius: 10px;
+          background-color: C(accent, 5%);
+          flex-direction: row;
+          flex-wrap: wrap;
+          align-items: center;
+
+          .titchy.button.tag {
+            flex-direction: row;
+            align-items: center;
+            gap: 0.5ch;
+            color: C(accent);
+            padding: 5px;
+            border-radius: 10px;
+            background-color: C(accent, 10%);
+
+            svg { @include size(1em); }
           }
 
-          &.small {
-            $g: 1ch;
-            justify-content: start;
-            min-width: fit-content;
-            gap: $g;
-            overflow: hidden;
+          .no-tag {
+            flex-direction: row;
+            align-items: center;
+            gap: 1ch;
+            color: C(secondary);
+            opacity: 0.3;
+            font-style: italic;
+          }
 
+          .sep { flex: 1; }
+        }
+      }
+    }
+
+    > .actions {
+      flex-direction: row;
+      gap: 0.75em;
+
+      .titchy.button {
+        svg { @include size(1.5em, 'all'); }
+
+        &.big {
+          width: 175px;
+        }
+
+        &.small {
+          $g: 1ch;
+          justify-content: start;
+          min-width: fit-content;
+          gap: $g;
+          overflow: hidden;
+
+          .title {
+            opacity: 0;
+            width: 0;
+            margin-inline-start: calc(-1 * $g);
+            white-space: nowrap;
+          }
+
+          &:hover {
             .title {
-              opacity: 0;
-              width: 0;
-              margin-inline-start: calc(-1 * $g);
-              white-space: nowrap;
-            }
-
-            &:hover {
-              .title {
-                opacity: 1;
-                width: auto;
-                margin-inline-start: 0;
-              }
+              opacity: 1;
+              width: auto;
+              margin-inline-start: 0;
             }
           }
         }
