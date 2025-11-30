d58afdf065c1787b1a0061c3fdf8df3641217265
Fastre <61952761+IamFastre@users.noreply.github.com>
2025-11-30 17:45:17 +0200

    feat: profile editing frontend stuff

--- src/routes/(app)/me/edit/+page.svelte
+++ src/routes/(app)/me/edit/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { redirect } from "@sveltejs/kit";
-  import { Button, Input, InputWrapper, Label, Option, Panel, Select, Separator, Textarea } from "titchy";
+  import { Button, Input, InputWrapper, Label, Option, Panel, Select, Separator, Textarea, useToaster } from "titchy";
   import { Pencil } from "@lucide/svelte";
 
   import { getLocale } from "@/paraglide/runtime";
   import { m } from "@/paraglide/messages";
-  import { COUNTRIES, entries } from "$/utils";
+  import { blobToDataURL, COUNTRIES, entries } from "$/utils";
+  import { ProfileEditForm } from "$/utils/zod/forms";
+  import { api } from "$/client/api";
 
   const { data } = $props();
   const user = $derived(data.user!);
@@ -13,17 +15,53 @@
   if (!data.user)
     redirect(303, '/auth');
 
+  const toaster = useToaster();
   const locale = getLocale();
 
+  let loading     = $state(false);
   let avatarFiles = $state<FileList>();
 
-  let avatar  = $derived<Blob | undefined>(avatarFiles?.[0]);
-  let display = $state(data.user.display_name ?? "");
-  let bio     = $state(data.user.bio ?? "");
-  let country = $state(data.user.country ?? "");
+  let avatarBlob = $derived<Blob | undefined>(avatarFiles?.[0]);
+  let display    = $state(data.user.display_name ?? "");
+  let bio        = $state(data.user.bio ?? "");
+  let country    = $state((data.user.country ?? "") as keyof typeof COUNTRIES);
+
+  const onResetAvatar = async () => {
+    loading = true;
+
+    const res = await api({ method:'POST', path:'/users/reset-avatar' });
+
+    if (res.error)
+      toaster.add({ type: 'error', content: m.generic_error_occurred() });
+    else
+      location.reload();
+
+    loading = false;
+  };
 
   const onSubmit = async () => {
-    alert("bleh...")
+    loading = true;
+    const avatar = avatarBlob ? await blobToDataURL(avatarBlob) : null;
+
+    const args: ProfileEditForm = {
+      avatar,
+      display_name: display,
+      bio,
+      country,
+    };
+
+    const { success, data } = ProfileEditForm.safeParse(args);
+
+    if (success) {
+      const res = await api({ method:'POST', path:'/users/edit', args:data });
+
+      if (res.error)
+        toaster.add({ type: 'error', content: m.generic_error_occurred() });
+      else
+        location.reload();
+
+      loading = false;
+    }
   };
 </script>
 
@@ -32,7 +70,7 @@
     <Label class="avatar" for="avatar">
       <Panel class="img-container" variant="secondary">
         <img
-          src={avatar ? URL.createObjectURL(avatar) : user.avatar}
+          src={avatarBlob ? URL.createObjectURL(avatarBlob) : user.avatar}
           alt="{user.username}'s avatar"
         />
         <div class="icon">
@@ -43,33 +81,39 @@
           type="file"
           id="avatar"
           accept="image/png,image/jpeg"
+          disabled={loading}
         />
       </Panel>
-      {#if avatar}
-        <Button variant="blank" onclick={e => { e.preventDefault(); avatar = undefined }}>
+      <div class="avatar-actions">
+        <Button variant="blank" onclick={onResetAvatar}  disabled={loading}>
           {m.generic_reset()}
         </Button>
-      {/if}
+        {#if avatarBlob}
+          <Button variant="blank" onclick={e => { e.preventDefault(); avatarBlob = undefined }} disabled={loading}>
+            {m.generic_deselect()}
+          </Button>
+        {/if}
+      </div>
     </Label>
 
     <Label class="displayName" for="displayName">
       <InputWrapper label={m.profile_display_name()}>
-        <Input bind:value={display} placeholder={m.profile_display_name_placeholder()} />
+        <Input bind:value={display} placeholder={m.profile_display_name_placeholder()} disabled={loading} />
       </InputWrapper>
     </Label>
 
     <Label class="bio" for="bio">
       <InputWrapper label={m.profile_bio()}>
-        <Textarea bind:value={bio} placeholder={m.profile_bio_placeholder()} />
+        <Textarea bind:value={bio} placeholder={m.profile_bio_placeholder()} disabled={loading} />
       </InputWrapper>
     </Label>
 
     <Panel class="field" variant="secondary">
       <Label class="country" for="country">
         <span class="title">{m.profile_country()}</span>
-        <Select bind:value={country} label="---" texts={{ deselect:m.generic_deselect(), details:"" }} deselectable copy-html>
+        <Select bind:value={country} label="---" texts={{ deselect:m.generic_deselect(), details:"" }} deselectable={!loading} copy-html>
           {#each entries(COUNTRIES) as [code, country], i (i)}
-            <Option class="country-option" value={code}>
+            <Option class="country-option" value={code} disabled={loading}>
               <img
                 src="https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/{code}.svg"
                 alt={country.emoji}
@@ -81,7 +125,7 @@
       </Label>
     </Panel>
     <Separator variant="secondary" line="dashed" />
-    <Button scaling={false} onclick={onSubmit}>
+    <Button scaling={false} onclick={onSubmit} disabled={loading}>
       {m.generic_save_changes()}
     </Button>
   </div>
@@ -137,26 +181,32 @@
         padding: 5px;
         @include flex-center;
         cursor: pointer;
-      }
 
-      input[type="file"] { display: none; }
+        input[type="file"] { display: none; }
 
-      img {
-        border-radius: 5px;
-        @include size(150px);
-      }
-
-      .icon {
-        position: absolute;
-
-        backdrop-filter: blur(5px);
-        background-color: C(primary, 75%);
-        padding: 10px;
-        border-radius: 50%;
-
-        svg {
-          @include size(2em);
+        img {
+          border-radius: 5px;
+          @include size(150px);
         }
+
+        .icon {
+          position: absolute;
+
+          backdrop-filter: blur(5px);
+          background-color: C(primary, 75%);
+          padding: 10px;
+          border-radius: 50%;
+
+          svg {
+            @include size(2em);
+          }
+        }
+      }
+
+      .avatar-actions {
+        flex-direction: row;
+        align-items: center;
+        gap: 10px;
       }
     }
   }
